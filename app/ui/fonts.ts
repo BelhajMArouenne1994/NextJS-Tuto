342824170fee5496527b46e913f4b70b2f99ee41
BelhajMArouenne1994 <belhajmarwen5@gmai.com>
2026-03-10 13:19:51 +0100

    Ajout des fichiers .env.example et .gitignore

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter } from 'next/font/google';
+import { Roboto } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const roboto = Roboto({ weight: ["400", "900"], subsets: ['cyrillic'] });
+export const lusitana = Lusitana({weight: ["400", "700"], subsets: ['latin']});
